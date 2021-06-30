232a8387c0d1492f20cf83ea99dda55181e0c48f
Oli <oliverrsaxon@gmail.com>
2021-06-30 19:32:55 +0100

    -

---
+++ schemas/documents/category.js
@@ -0,0 +1,22 @@
+export default {
+    title: 'Category',
+    name: 'category',
+    type: 'document',
+    fields: [
+        {
+            title: 'Title',
+            name: 'title',
+            type: 'string',
+        },
+
+        {
+            title: 'Slug',
+            name: 'slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                auto: true,
+            }
+        }
+    ]
+}
---
+++ schemas/documents/ingredient.js
@@ -0,0 +1,17 @@
+export default {
+    title: 'Ingredient',
+    name: 'ingredient',
+    type: 'document',
+    fields: [
+        {
+            title: 'Title',
+            name: 'title',
+            type: 'string',
+        },
+        {
+            title: '1 Cup in Grams',
+            name: 'cupInGrams',
+            type: 'number',
+        }
+    ]
+}
